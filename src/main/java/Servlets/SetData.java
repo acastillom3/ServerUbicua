91d8226edf8f9cfecb3ac0310c6c59b5eddd4d0f
acastillom <31887044+acastillom3@users.noreply.github.com>
2025-11-23 17:45:54 +0100

    Updated pom and change connection to database

--- src/main/java/Servlets/SetData.java
+++ src/main/java/Servlets/SetData.java
@@ -21,28 +21,27 @@ public class SetData extends HttpServlet
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		Log.log.info("--Get values from the DB--");
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		try {
-			int value = Integer.parseInt(request.getParameter("value"));
-			//Logic.setDataToDB(value);
-		} catch (NumberFormatException nfe){
-			out.println("-1");
-			Log.log.error("Number Format Exception: " + nfe);
-		} catch (IndexOutOfBoundsException iobe) {
-			out.println("-1");
-			Log.log.error("Index out of bounds Exception: " + iobe);
-		} catch (Exception e){
-			out.println("-1");
-			Log.log.error("Exception: " + e);
-		} finally{
-			out.close();
-		}
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        Log.log.info("--Get values from the DB--");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            int value = Integer.parseInt(request.getParameter("value"));
+            Logic.setDataToDB(value);
+        } catch (NumberFormatException nfe){
+            out.println("-1");
+            Log.log.error("Number Format Exception: " + nfe);
+        } catch (IndexOutOfBoundsException iobe) {
+            out.println("-1");
+            Log.log.error("Index out of bounds Exception: " + iobe);
+        } catch (Exception e){
+            out.println("-1");
+            Log.log.error("Exception: " + e);
+        } finally{
+            out.close();
+        }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            doGet(request, response);
+    }
 }
