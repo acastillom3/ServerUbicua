b46566973c4c22328671fd70115c9a22378fdd0b
acastillom <31887044+acastillom3@users.noreply.github.com>
2025-11-24 12:01:29 +0100

    Update MQTTBroker.java

--- src/main/java/Mqtt/MQTTBroker.java
+++ src/main/java/Mqtt/MQTTBroker.java
@@ -3,10 +3,10 @@ package mqtt;
 public class MQTTBroker {
 
     private static int qos = 2;
-    private static final String broker = "tcp://localhost:1883";
+    private static final String broker = "tcp://mqtt-broker:1883";
     private static final String clientId = "WheatherStationUAH";
-    private static final String username = "ubicua";
-    private static final String password = "ubicua";
+    private static final String username = "";
+    private static final String password = "";
     
     public MQTTBroker() {
     }
@@ -30,5 +30,11 @@ public class MQTTBroker {
     public static String getPassword() {
         return password;
     }
-    
+    public static String getSubscriberClientId() {
+        return clientId + "-subscriber";
+    }
+
+    public static String getPublisherClientId() {
+        return clientId + "-publisher";
+    }
 }
