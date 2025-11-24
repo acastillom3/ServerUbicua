5e54e4b60f4cdcb79a94046e27fa3f521b978bb5
acastillom <31887044+acastillom3@users.noreply.github.com>
2025-11-24 12:02:26 +0100

    Refactor MQTTSuscriber for improved connection handling

--- src/main/java/Mqtt/MQTTSuscriber.java
+++ src/main/java/Mqtt/MQTTSuscriber.java
@@ -1,11 +1,6 @@
 package mqtt;
 
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import Database.Topics;
@@ -13,36 +8,49 @@ import logic.Log;
 
 public class MQTTSuscriber implements MqttCallback {
 
-    public void suscribeTopic(MQTTBroker broker, String topic) {
-        Log.logmqtt.debug("Suscribe to topics");
-        MemoryPersistence persistence = new MemoryPersistence();
-        try {
-            MqttClient sampleClient = new MqttClient(MQTTBroker.getBroker(), MQTTBroker.getClientId(), persistence);
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setUserName(MQTTBroker.getUsername());
-            connOpts.setPassword(MQTTBroker.getPassword().toCharArray());
-            connOpts.setCleanSession(true);
-            Log.logmqtt.debug("Mqtt Connecting to broker: " + MQTTBroker.getBroker());
-            sampleClient.connect(connOpts);
-            Log.logmqtt.debug("Mqtt Connected");
-            sampleClient.setCallback(this);
+    private MqttClient client;
+    private String brokerUrl;
+    private String clientId;
+    private String username;
+    private String password;
 
-            sampleClient.subscribe(topic);
+    public MQTTSuscriber(MQTTBroker broker) {
+        this.brokerUrl = broker.getBroker();
+        this.clientId = broker.getClientId();
+        this.username = broker.getUsername();
+        this.password = broker.getPassword();
+    }
+
+    public void subscribeTopic(String topic) {
+        try {
+            MemoryPersistence persistence = new MemoryPersistence();
+            client = new MqttClient(brokerUrl, MQTTBroker.getSubscriberClientId(), persistence);
+
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setUserName(username);
+            connOpts.setPassword(password.toCharArray());
+            connOpts.setCleanSession(false); // Para mantener la suscripción
+            connOpts.setAutomaticReconnect(true); // Reconexión automática
+            connOpts.setConnectionTimeout(10);
+
+            client.setCallback(this);
+            client.connect(connOpts);
+
+            client.subscribe(topic, 1); // QoS 1 para asegurarse de recibir
             Log.logmqtt.info("Subscribed to {}", topic);
 
-        } catch (MqttException me) {
-            Log.logmqtt.error("Error suscribing topic: {}", me);
-        } catch (Exception e) {
-            Log.logmqtt.error("Error suscribing topic: {}", e);
+        } catch (MqttException e) {
+            Log.logmqtt.error("Error subscribing to topic: {}", e);
         }
     }
 
     @Override
     public void connectionLost(Throwable cause) {
+        Log.logmqtt.warn("MQTT Connection lost, cause: {}", cause.getMessage());
     }
 
     @Override
-    public void messageArrived(String topic, MqttMessage message) throws Exception {
+    public void messageArrived(String topic, MqttMessage message) {
         Log.logmqtt.info("{}: {}", topic, message.toString());
         Topics newTopic = new Topics();
         newTopic.setValue(message.toString());
